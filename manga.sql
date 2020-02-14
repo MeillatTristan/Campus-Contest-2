dd1924724870dd618962f024b78eb3da9a758e25
elfepee <tristan.meillat28@gmail.com>
2020-02-14 09:20:11 +0100

    modif bdd

--- manga.sql
+++ manga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 14 fév. 2020 à 08:18
+-- Généré le :  ven. 14 fév. 2020 à 08:19
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -46,6 +46,63 @@ INSERT INTO `livre` (`id`, `tome`, `stock`, `serieID`) VALUES
 (1, 1, 5, 1),
 (2, 1, 20, 2),
 (3, 2, 25, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mangaID` int(11) NOT NULL,
+  `emprunteurID` int(11) NOT NULL,
+  `dateEmprunt` date NOT NULL,
+  `dateRetour` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `series`
+--
+
+DROP TABLE IF EXISTS `series`;
+CREATE TABLE IF NOT EXISTS `series` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `date_sortie` date NOT NULL,
+  `auteur` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `series`
+--
+
+INSERT INTO `series` (`id`, `name`, `date_sortie`, `auteur`) VALUES
+(1, 'One Piece', '1997-07-22', 'Eichiro Oda'),
+(2, 'Naruto', '1999-09-20', 'Masashi Kishimoto');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `admin` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `numéro` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
