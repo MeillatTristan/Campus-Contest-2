dc3ef986c96620cd7daa04c2fecb2d903ca5fd20
elfepee <tristan.meillat28@gmail.com>
2020-02-15 13:15:14 +0100

    bdd

--- manga.sql
+++ manga.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 15 fév. 2020 à 11:20
+-- Généré le :  sam. 15 fév. 2020 à 12:14
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
