72b98a405d29d41ac3da375ba9b76bdd7af69fc4
CharlesR <ranouilcharles@gmail.com>
2020-02-17 10:49:05 +0100

    form incription index

--- connexion.php
+++ connexion.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/connexion.css">
   <title>connexion</title>
 </head>
 <body>
@@ -12,15 +13,19 @@
       header('Location: index.php');
     }
   ?>
-  <h2>Connexion</h2>
+  <h2 class="title">Connexion</h2>
 
-  <form action="authUser.php" method="post">
+  <form class="formulaire" action="authUser.php" method="post">
 
-    <label for="mail">Email :</label>
-    <input type="text" id="mail" name="email">
+    <p class="box">
+      <label for="mail">Email :</label>
+      <input type="text" id="mail" name="email">
+    </p>
 
-    <label for="password">Mot de Passe (8 caractère minimum):</label>
-    <input type="password" id="password" name="password" minlength="8" required>
+    <p class="box">
+      <label for="password">Mot de Passe (8 caractère minimum):</label>
+      <input type="password" id="password" name="password" minlength="8" required>
+    </p>
 
     <input type="submit" value="Valider">
   </form>
