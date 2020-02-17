11cb5918f8ffc406c24bb20a814639e75bbfcc27
elfepee <tristan.meillat28@gmail.com>
2020-02-17 22:00:41 +0100

    ajout gestion des emprunts

--- empruntsView.php
+++ empruntsView.php
@@ -7,8 +7,69 @@
   <title>Document</title>
 </head>
 <body>
+<?php
+session_start();
+
+if(!isset($_SESSION['id'])){
+    echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
+}
+else{
+  include 'configbdd.php';
+
+  $idUsers =  $_SESSION['id'];
+  $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
+  if ($admin->fetch()[0] != 'y'){
+      echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
+
+  }
+  else{
+  ?>
   <h3>Les emprunts en cours</h3>
- <table>
+  <table>
+    <td>Emprunteur</td>
+    <td>Série </td>
+    <td>Tome </td>
+    <td>Date d'emprunt </td>
+    <td>Date de retour </td>
+    <td>Livre retourner</td>
+    <td>mail</td>
+    <?php
+    include "configbdd.php";
+    $locations = $bdd->query("SELECT * FROM locations");
+    while ($location = $locations->fetch()){
+      $idCustomer = $location['customersID'];
+      $customer = $bdd->query("SELECT * FROM users WHERE id = $idCustomer")->fetch();
+      if ($location['returnBool'] == 'n'){
+        $idLocationSerie = $location['serieID'];
+        $idLocation = $location['id'];
+        $nameSerie = $bdd->query("SELECT name FROM series WHERE id = $idLocationSerie")->fetch()[0];
+        $tome = $location['tome'];
+        $borrowingDate = $location['borrowingDate'];
+        $returnDate = $location['returnDate'];
+        $mailCustomer = $customer['email'];
+        $textMail = "Bonjour, %0D%0A vous avez louer le tome $tome de $nameSerie le $borrowingDate, vous étiez sensé le rendre le $returnDate.  %0D%0A Merci de le rapporter en magasin le plus vite possible. %0D%0A Bonne journée, %0D%0A l`équipe Manga++";
+
+        ?>
+          <tr>
+            <td> <?php echo $customer['name']." ".$customer['firstname'] ?> </td>
+            <td><?php echo $nameSerie?> </td>
+            <td><?php echo $tome?> </td>
+            <td><?php echo $borrowingDate?> </td>
+            <td><?php echo $returnDate?> </td>
+            <?php
+            echo "<td> <a href='retourLivre.php?idLocation=$idLocation'>livre rendu</a></td>";
+            if (date("Y-m-d") > $returnDate){
+              echo "<td><a href='mailto:$mailCustomer?subject=Manga++ : retour livre&body=$textMail'>envoyer un mail au loueur</a></td>";
+            }
+      }
+            ?>
+          </tr>
+    </table>
+  <?php
+  }
+  ?>
+  <h3>Les emprunts en cours</h3>
+  <table>
     <td>Emprunteur</td>
     <td>Série </td>
     <td>Tome </td>
@@ -20,7 +81,7 @@
     while ($location = $locations->fetch()){
       $idCustomer = $location['customersID'];
       $customer = $bdd->query("SELECT * FROM users WHERE id = $idCustomer")->fetch();
-      if ($location['returnBool'] == 'n'){
+      if ($location['returnBool'] == 'y'){
         $idLocationSerie = $location['serieID'];
         $nameSerie = $bdd->query("SELECT name FROM series WHERE id = $idLocationSerie")->fetch()[0];
         $tome = $location['tome'];
@@ -34,16 +95,14 @@
             <td><?php echo $borrowingDate?> </td>
             <td><?php echo $returnDate?> </td>
             <?php
-            if (date("Y-m-d") > $returnDate){
-              echo "<td> le livre doit être ramener ! </td>";
-            }
       }
             ?>
           </tr>
-  </table>
+    </table>
   <?php
-    
   }
+}
+}
   ?>
 </body>
 </html>
---
+++ retourLivre.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['id'])){
+    echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
+}
+else{
+  include 'configbdd.php';
+
+  $idUsers =  $_SESSION['id'];
+  $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
+  if ($admin->fetch()[0] != 'y'){
+      echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
+
+  }
+  else{
+    $idLocation = $_REQUEST['idLocation'];
+    $bdd->query("UPDATE locations SET returnBool = 'y' WHERE id = $idLocation");
+    header("Location:empruntsView.php");
+  }
+}
+?>
