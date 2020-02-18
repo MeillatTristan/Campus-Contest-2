9eee9e74135fa25b38a22ce0b2adefcfc4bb062d
CharlesR <ranouilcharles@gmail.com>
2020-02-18 16:18:57 +0100

    indentation 4

--- retourLivre.php
+++ retourLivre.php
@@ -1,22 +1,22 @@
 <?php
-session_start();
-echo "yo";
-if(!isset($_SESSION['id'])){
-    echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
-}
-else{
-  include 'configbdd.php';
-
-  $idUsers =  $_SESSION['id'];
-  $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
-  if ($admin->fetch()[0] != 'y'){
+  session_start();
+  echo "yo";
+  if(!isset($_SESSION['id'])){
       echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
-
   }
   else{
-    $idLocation = $_REQUEST['idLocation'];
-    $bdd->query("UPDATE locations SET returnBool = 'y' WHERE id = $idLocation");
-    header("Location:pageAdminGestionEmprunts.php");
+    include 'configbdd.php';
+
+    $idUsers =  $_SESSION['id'];
+    $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
+    if ($admin->fetch()[0] != 'y'){
+        echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
+
+    }
+    else{
+      $idLocation = $_REQUEST['idLocation'];
+      $bdd->query("UPDATE locations SET returnBool = 'y' WHERE id = $idLocation");
+      header("Location:pageAdminGestionEmprunts.php");
+    }
   }
-}
 ?>
