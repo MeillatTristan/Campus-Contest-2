c286df198edb21a441cf645f2b835d52003d662c
CharlesR <ranouilcharles@gmail.com>
2020-02-18 16:10:03 +0100

    indentation 3

--- myAccount.php
+++ myAccount.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="assets/css/profil.css">
-  <title>Mon compte</title>
-</head>
-<body>
-  <?php
-    include "configbdd.php";
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/profil.css">
+    <title>Mon compte</title>
+  </head>
+  <body>
+    <?php
+      include "configbdd.php";
 
-    if(!isset($_SESSION['id'])){
-      echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
-    }
-    else{
-      $idUsers = $_SESSION['id'];
-      $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
-      if ($admin->fetch()[0] == 'y' && isset($_REQUEST['idMaj'])){
-        $idToMaj = $_REQUEST['idMaj'];
+      if(!isset($_SESSION['id'])){
+        echo "veuillez vous connectez en tant qu'admin pour accéder à cette page";
       }
       else{
-        $idToMaj = $idUsers;
-      }
-      $idUsers = $_SESSION['id'];
-      $locations = $bdd->query("SELECT * FROM locations WHERE customersID = $idToMaj");
-      echo "<a class='updateinfos' href='updateInfos.php?idMaj=".$idToMaj."'>Modifier mes infos</a>";
-      ?>
-      <h2>Mes commandes</h2>
-      <?php
+        $idUsers = $_SESSION['id'];
+        $admin = $bdd->query("SELECT admin FROM users WHERE id = $idUsers" );
+        if ($admin->fetch()[0] == 'y' && isset($_REQUEST['idMaj'])){
+          $idToMaj = $_REQUEST['idMaj'];
+        }
+        else{
+          $idToMaj = $idUsers;
+        }
+        $idUsers = $_SESSION['id'];
+        $locations = $bdd->query("SELECT * FROM locations WHERE customersID = $idToMaj");
+        echo "<a class='updateinfos' href='updateInfos.php?idMaj=".$idToMaj."'>Modifier mes infos</a>";
+    ?>
+    <h2>Mes commandes</h2>
+    <?php
       echo "<h3>Commandes en cours</h3>";
       echo "<table>";
       echo "<td>Série </td>";
@@ -43,20 +43,20 @@
           $tome = $location['tome'];
           $borrowingDate = $location['borrowingDate'];
           $returnDate = $location['returnDate'];
-          ?>
-            <tr>
-              <td><?php echo $nameSerie?> </td>
-              <td><?php echo $tome?> </td>
-              <td><?php echo $borrowingDate?> </td>
-              <td><?php echo $returnDate?> </td>
-              <?php
-              if (date("Y-m-d") > $returnDate){
-                echo "<td> Vous devez ramener votre livre !</td>";
-              }
-              ?>
-            </tr>
-          </table>
-          <?php
+    ?>
+    <tr>
+      <td><?php echo $nameSerie?> </td>
+      <td><?php echo $tome?> </td>
+      <td><?php echo $borrowingDate?> </td>
+      <td><?php echo $returnDate?> </td>
+      <?php
+        if (date("Y-m-d") > $returnDate){
+          echo "<td> Vous devez ramener votre livre !</td>";
+        }
+      ?>
+    </tr>
+    </table>
+    <?php
         }
       }
       echo "<h3>Commandes terminées</h3>";
@@ -73,18 +73,18 @@
           $tome = $location['tome'];
           $borrowingDate = $location['borrowingDate'];
           $returnDate = $location['returnDate'];
-          ?>
-            <tr>
-              <td><?php echo $nameSerie?> </td>
-              <td><?php echo $tome?> </td>
-              <td><?php echo $borrowingDate?> </td>
-              <td><?php echo $returnDate?> </td>
-            </tr>
-          </table>
-          <?php
-        }
+    ?>
+      <tr>
+        <td><?php echo $nameSerie?> </td>
+        <td><?php echo $tome?> </td>
+        <td><?php echo $borrowingDate?> </td>
+        <td><?php echo $returnDate?> </td>
+      </tr>
+    </table>
+    <?php
+       }
+      }
     }
-  }
-  ?>
-</body>
+    ?>
+  </body>
 </html>
--- profil.php
+++ profil.php
@@ -18,48 +18,48 @@
 		?>
 
 		<!-- Header -->
-			<header id="header">
-				<div class="logo"><a href="#">page profil</a></div>
-				<li><a href="index.php">Accueil</a></li>
-				<li><a href="deconnexion.php">Déconnexion</a></li>
-			</header>
+		<header id="header">
+			<div class="logo"><a href="#">page profil</a></div>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="deconnexion.php">Déconnexion</a></li>
+		</header>
 
 		<!-- Main -->
-			<section id="main">
-				<div class="inner">
+		<section id="main">
+			<div class="inner">
 
-				<!-- One -->
-					<section id="one" class="wrapperStyle1">
-					<?php include 'myAccount.php' ?>
-					</section>
+			<!-- One -->
+				<section id="one" class="wrapperStyle1">
+				<?php include 'myAccount.php' ?>
+				</section>
 
-				<!-- Two -->
-					<section id="two" class="wrapperStyle2">
-					<?php //include 'location.php' ?>
-					</section>
+			<!-- Two -->
+				<section id="two" class="wrapperStyle2">
+				<?php //include 'location.php' ?>
+				</section>
 
-				<!-- Three -->
-					<section id="three" class="wrapper">
-					</section>
-				</div>
-				<div class="biblioLink">
-					<a href="bibliotheque.php">Aller à la bibliothèque</a>
-				</div>
-			</section>
+			<!-- Three -->
+				<section id="three" class="wrapper">
+				</section>
+			</div>
+			<div class="biblioLink">
+				<a href="bibliotheque.php">Aller à la bibliothèque</a>
+			</div>
+		</section>
 
 		<!-- Footer -->
-			<footer id="footer">
-				<div class="container">
-					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
-					</ul>
-				</div>
-				<div class="copyright">
-					&copy; Untitled. All rights reserved. Images <a href="https://unsplash.com">Unsplash</a> Design <a href="https://templated.co">TEMPLATED</a>
-				</div>
-			</footer>
+		<footer id="footer">
+			<div class="container">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				</ul>
+			</div>
+			<div class="copyright">
+				&copy; Untitled. All rights reserved. Images <a href="https://unsplash.com">Unsplash</a> Design <a href="https://templated.co">TEMPLATED</a>
+			</div>
+		</footer>
 	</body>
 </html>
